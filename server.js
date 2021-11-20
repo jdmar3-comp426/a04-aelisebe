54e6f04afcdc7adfa5c0c28f35c40e8f9d3c7158
aelisebe <59695688+aelisebe@users.noreply.github.com>
2021-11-20 16:56:28 -0500

    change endpoint mssg

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/", (req, res) => {	
 	
 	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?,?)").run(req.body.user, md5(req.body.pass));
-	res.status("201").json(stmt);
+	res.status("201").json({'message': "1 record created: ID %id% (201)".replace("%id%", stmt.lastInsertRowid)});
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users/", (req, res) => {	
@@ -44,7 +44,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id=?").run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).json(stmt);
+	res.status(200).json({'message': "1 record created: ID %id% (200)".replace("%id%", stmt.lastInsertRowid)});
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
